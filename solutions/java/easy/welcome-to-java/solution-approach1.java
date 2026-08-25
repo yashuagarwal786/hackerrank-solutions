1937bcc42ab7b4acad0255cd2f4b134aceb60d78
Yash Agarwal <yashuagarwal0786@gmail.com>
2026-08-25 20:29:19 +0530

    ✅ Welcome to Java! [java] — approach1

---
+++ solutions/java/easy/welcome-to-java/solution-approach1.java
@@ -0,0 +1,20 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+// Problem     Welcome to Java!
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-25, 08:29 p.m.
+// ──────────────────────────────────────────────────
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
