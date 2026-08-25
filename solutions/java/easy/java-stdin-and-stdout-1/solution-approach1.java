bf8e771401a692d5da5989deea2b0fb814c40a67
Yash Agarwal <yashuagarwal0786@gmail.com>
2026-08-25 20:29:10 +0530

    ✅  Java Stdin and Stdout I [java] — approach1

---
+++ solutions/java/easy/java-stdin-and-stdout-1/solution-approach1.java
@@ -0,0 +1,28 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
+// Problem      Java Stdin and Stdout I
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-25, 08:29 p.m.
+// ──────────────────────────────────────────────────
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+    
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        
+    }
+}
